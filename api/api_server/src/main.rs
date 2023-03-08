5dba5e2bcfd9df798aed75b72a82d678eaf42332
Keith <youngka@gmail.com>
2023-03-08 15:36:33 +0000

    added api server code

--- api/api_server/src/main.rs
+++ api/api_server/src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::web::Data;
+//use std::net::IpAddr;
+
+#[get("/time")]
+async fn get_time() -> impl Responder {
+    let now = chrono::Local::now();
+    HttpResponse::Ok().body(format!("{}", now))
+}
+
+//#[get("/ip")]
+// async fn get_ip(addr: actix_web::web::Addr<std::net::IpAddr>) -> impl Responder {
+//     let ip = addr.get_ref().clone();
+//     HttpResponse::Ok().body(format!("{}", ip))
+// }
+
+
+
+#[get("/add/{num1}/{num2}")]
+async fn add_numbers(info: actix_web::web::Path<(i32, i32)>) -> impl Responder {
+    let (num1, num2) = info.into_inner();
+    let result = num1 + num2;
+    HttpResponse::Ok().body(format!("{}", result))
+}
+
+#[get("/roman/{num}")]
+async fn roman(info: actix_web::web::Path<i32>) -> impl Responder {
+    let num = info.into_inner();
+    // Define Roman numeral symbols and their values
+    let symbols = vec![
+        ('M', 1000),
+        ('D', 500),
+        ('C', 100),
+        ('L', 50),
+        ('X', 10),
+        ('V', 5),
+        ('I', 1),
+    ];
+
+    // Build Roman numeral string
+    let mut result = String::new();
+    let mut remaining = num;
+    for &(symbol, value) in symbols.iter() {
+        while remaining >= value {
+            result.push(symbol);
+            remaining -= value;
+        }
+
+        // Check for subtractive notation
+        if remaining > 0 {
+            let next_value = symbols.iter().find(|&&(_, v)| v <= remaining);
+            if let Some(&(next_symbol, next_value)) = next_value {
+                let difference = value - next_value;
+                if difference <= remaining {
+                    result.push(next_symbol);
+                    result.push(symbol);
+                    remaining -= difference;
+                }
+            }
+        }
+    }
+
+
+    HttpResponse::Ok().body(format!("{}", result))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Bind the server to localhost:8080
+    HttpServer::new(|| {
+        let ip = std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1));
+        App::new()
+            .app_data(Data::new(ip))
+            .service(get_time)
+            //.service(get_ip)
+            .service(add_numbers)
+            .service(roman)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
